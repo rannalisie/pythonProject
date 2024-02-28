4afa6c4de7b142d3bd8941368601d5810d56d6fc
Ranna Lisie <105607854+rannalisie@users.noreply.github.com>
2024-02-27 22:40:19 -0300

    tuplas

---
+++ dados_tuplas/funcoes.py
@@ -0,0 +1,11 @@
+def perguntar () :
+    return ("O que deseja realizar\n +"
+              "<I> - Para Inserir um usuario\n"+
+              "<P> - Para Pesquisar um usuario\n"+
+              "<E> - Para Ecluir um usuario\n"+
+              "<L> - Para Listar um usuario: ").upper()
+
+def inserir(dicionario):
+    dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
+                                                   input("Digite a ultima data de acesso: "),
+                                                   input("Qual a ultima estação acessada: ").upper()]
---
+++ dados_tuplas/pacotes.py
@@ -0,0 +1,8 @@
+from dados_tuplas.funcoes import *
+
+usuarios = {}
+opcao = perguntar()
+while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
+    if opcao=="I":
+        inserir(usuarios)
+    opcao = perguntar()
